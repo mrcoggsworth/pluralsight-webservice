17a29fd3e01518a77299727aafe738dba3760f8f
Christopher Scogin <mrcoggsworth@gmail.com>
2021-08-03 13:24:23 -0500

    adding builtin package and user struct

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/mrcoggsworth/pluralsight-webservice/models"
 )
 
-func main(){
-	fmt.Println("Hello from the go world at pluralsight...")
+func main() {
+	u := models.User{
+		ID:        1,
+		FirstName: "Chris",
+		LastName:  "Scogin",
+	}
+
+	fmt.Println(u)
+
 }
